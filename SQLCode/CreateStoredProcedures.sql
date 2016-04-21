2f8a6d4c9f30b40cd3a37b863c32cd14de4da73e
James Coleman Gibson <colemanjones@gmail.com>
2016-04-21 11:41:35 -0400

    Missed stored procedures in last commit

--- SQLCode/CreateStoredProcedures.sql
+++ SQLCode/CreateStoredProcedures.sql
@@ -13,7 +13,7 @@ VALUES
 (@Uname, @pass, @email)
 
 Go
-exec insert_new_user @Uname="dummy", @pass="123", @email="a@a"
+exec insert_new_user @Uname="Coleman", @pass="123", @email="b@b"
 
 Go
 CREATE PROCEDURE insert_new_user_profile
@@ -24,11 +24,67 @@ CREATE PROCEDURE insert_new_user_profile
 @experience varchar(100),
 @userid int
 AS
+
 INSERT INTO USERPROFILE
 (Name, Gender, Major, Address, Experience)
 VALUES
 (@name, @gender, @major, @address, @experience)
 
+UPDATE DREAMCAREERUSER
+SET ProfileID = (SELECT SCOPE_IDENTITY())
+WHERE UserID = @userid
+
 Go
 exec insert_new_user_profile @name="Coleman Gibson", @gender="M",
-		@major="Computer Science", @address="underground", @experience="This"
+		@major="Computer Science", @address="underground", @experience="This",
+		@userid=3
+
+
+Go
+CREATE PROCEDURE insert_new_company
+@address varchar(20),
+@size int,
+@name varchar(20),
+@description varchar(100)
+AS
+INSERT INTO COMPANY
+(CompanyAddress, Size, Name, Description)
+VALUES
+(@address, @size, @name, @description)
+
+Go
+exec insert_new_company @address="Rose", @size=1000, @name="Coleman's Company", 
+	@description="This is a company"
+
+
+Go
+CREATE PROCEDURE insert_new_position
+@companyid int,
+@postype varchar(20),
+@posloc varchar(20),
+@salary money
+
+AS
+INSERT INTO POSITION
+(CompanyID, PositionType, PositionLocation, Salary)
+VALUES
+(@companyid, @postype, @posloc, @salary)
+
+Go
+exec insert_new_position @companyid=1, @postype="Internship",
+	@posloc="Terre Haute", @salary=50000
+
+
+--Go
+--CREATE PROCEDURE insert_position_tag
+--@tagtext varchar(20),
+--@posid int
+
+--AS
+--INSERT INTO TAG
+--(TagWord, )
+--VALUES
+--(@tagtext)
+
+--UPDATE POSITION
+--SET 
