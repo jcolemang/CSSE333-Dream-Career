bb01945b1c58cb52492a9e21a6b594878dc51cdd
Aaradhana96 <bharila@rose-hulman.edu>
2016-04-29 08:52:30 -0400

    changes

--- DreamCareer/scripts/CreateProfile.js
+++ DreamCareer/scripts/CreateProfile.js
@@ -1,30 +1,11 @@
-﻿
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-/*function myFunction()
-{
-	$('#myDropdown').show();
-	return false;
-}
-
-
-	// Close the dropdown menu if the user clicks outside of it
-	window.onclick = function (event) {
-		if (!event.target.matches('.dropbtn')) {
-
-			var dropdowns = document.getElementsByClassName("dropdown-content");
-			var i;
-			for (i = 0; i < dropdowns.length; i++) {
-				var openDropdown = dropdowns[i];
-				$(openDropdown).hide();
-			}
-		}
-	}  */
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $('.gender-select').change(function () {
         genderCheck();
+    })
+})
+$(document).ready(function () {
+    $('.prof-username').change(function () {
+        usernameExistCheck();
     })
 })
 
@@ -35,7 +16,16 @@ function genderCheck() {
 	    $('#gender-not-selected-error').show();
 	    return false;
 	}
-
 	$('#gender-not-selected-error').hide();
 	return true;
 }
+/*function usernameExistCheck() {
+      
+    // if () {
+   
+        $('#username-input-error-label')
+            .text('Passwords do not match');
+        $('#username-input-error-label').show();
+        return false;
+   // }
+}    */
