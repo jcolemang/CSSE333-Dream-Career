29fb28da0bc6adb603daee58777cb5d1e9fb5b5e
James Coleman Gibson <colemanjones@gmail.com>
2016-05-03 18:33:11 -0400

    Added SQL Procedures that search by position tag. Modified a file which inserts new tags.

--- SQLCode/Stored Procedures/insert_new_tag.sql
+++ SQLCode/Stored Procedures/insert_new_tag.sql
@@ -6,6 +6,14 @@ GO
 CREATE PROCEDURE insert_new_tag
 	(@tagword varchar(20))
 AS
+	DECLARE @RepeatTagError smallint
+	SET @RepeatTagError = -1
+
+	IF EXISTS (SELECT * FROM Tag WHERE Tag.TagWord=@tagword)
+	BEGIN
+		PRINT 'Tags must be unique.'
+		RETURN @RepeatTagError
+	END
 
 	INSERT INTO Tag
 	(TagWord)
---
+++ SQLCode/Stored Procedures/search_by_tags.sql
@@ -0,0 +1,41 @@
+USE DreamCareer
+GO
+
+-- A table type
+
+CREATE TYPE TagWordsTableType as TABLE
+(
+	TagWords varchar(20)
+)
+
+
+GO
+CREATE PROCEDURE search_by_tags
+	(@Tags AS TagWordsTableType READONLY)
+AS	
+	-- Used later
+	DECLARE @NumTags int
+	SET @NumTags = (SELECT COUNT(*) FROM @Tags)
+	
+	SELECT Position.PositionTitle AS Title, Position.PositionType AS PositionType,
+			Position.PositionDescription AS PositionDescription, 
+			Position.Salary AS Salary
+	FROM Position, HasTag, Tag
+	WHERE Position.PositionID = HasTag.PositionID AND
+			HasTag.TagID = Tag.TagID
+
+	-- Extra groups so I can select those columns
+	GROUP BY Position.PositionID, PositionTitle, PositionType, 
+			PositionDescription, Salary
+
+	-- I can only use this because the actual tag words are unique
+	HAVING COUNT(Position.PositionID) >= @NumTags
+	
+	
+
+	
+
+
+
+
+
